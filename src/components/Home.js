f504c7d013ef33dbe3909a9341cd928639e55ef4
unknown <jaskirat2357@gmail.com>
2023-01-21 23:47:12 +0530

    react

---
+++ src/components/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import jass6 from './images/jass6.jpeg';
+import {FaLinkedin,FaGithubSquare,FaInstagramSquare,FaMailBulk } from "react-icons/fa";
+
+
+const Home = () => {
+return(
+   
+  
+    <div className='Home'>
+       
+
+  
+
+        <p style={{color:"red",fontSize:"160%"}}>Hi,my name is</p>
+    <h1 style={{fontSize:"360%"}}>Jaskirat Kaur Chhabra</h1>
+    <h3 >I'm a Developer</h3>
+    <p style={{color:"black",fontSize:"160%",fontStyle:'Italic'}}> I like to make new and responsive websites that are user friendly... </p>
+    <button className="but">View Work →</button>
+    <div className='Home1'>
+    <img src={jass6} alt="this is car image" /></div>
+    <div className='icons'>
+    <FaLinkedin style={{color:"blue",fontSize:"260%",marginLeft:"-25%"}}/>
+     <FaGithubSquare style={{color:"black",fontSize:"260%",marginLeft:"-25%"}}/>
+     <FaInstagramSquare style={{color:"rgba(246, 43, 215, 0.941)",fontSize:"260%",marginLeft:"-25%"}}/>
+     <FaMailBulk style={{color:"black",fontSize:"300%",marginLeft:"-25%"}}/>
+    </div>
+    </div>
+)
+}
+export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React,{useState,useEffect} from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  const [toggleMenu,setToggleMenu]=useState(false)
+  const[screenWidth,setScreenWidth]=useState(window.innerWidth)
+  const toggleNav=()=>{
+    setToggleMenu(!toggleMenu)
+  }
+useEffect(()=>{
+const changeWidth=()=>{
+  setScreenWidth(window.innerWidth)
+}
+window.addEventListener('resize',changeWidth)
+
+
+},[])
+  return (
+    <div className='container'>
+        <nav>
+          {(toggleMenu || screenWidth>500) && (
+            <ul className='list'>
+                <li className='items'>Home</li>
+                <li className='items'>About</li>
+                <li className='items'>Skills</li>
+                <li className='items'>Work</li>
+                <li className='items'>Contact</li>
+                
+            </ul>)}
+            <button onClick={toggleNav} className='btn'>Btn</button>
+       
+        </nav>
+        </div>
+  );
+
+}
+
+export default Navbar;
