edd7bc074e683e17da0425d75f09a6a695a6a773
unknown <jaskirat2357@gmail.com>
2023-01-24 22:50:37 +0530

    contact responsive

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,16 +8,15 @@ const Contact = () => {
         <div >
         <hr style={{fontWeight:"bold"}}></hr>
         <div style={{width:"100%",padding:"50px",backgroundColor:"rgb(27, 16, 74)",marginBottom:"70%"}} className='Contact'>
-            <h1 style={{color:"white",marginLeft:"40%"}}>CONTACT ME</h1>
+            <h1 style={{color:"white",marginLeft:"40%"}} className="bo">CONTACT ME</h1>
             </div>
-
             <div>
             <h1 style={{marginLeft:"10%",marginTop:"-60%",marginBottom:"70%"}}>Let's chat.</h1>
             <h1 style={{marginLeft:"10%",marginTop:"-70%",marginBottom:"70%"}}>Tell me about your projects</h1>
             <p style={{marginLeft:"10%",marginTop:"-68%",marginBottom:"70%",fontWeight:"bold"}}>Let's create something together</p>
             <h4 style={{marginLeft:"20%",marginTop:"-68%",marginBottom:"70%"}}>Mail me at</h4>
             <p style={{marginLeft:"20%",marginTop:"-70%",marginBottom:"40%",color:"purple",fontWeight:"bold"}}>jaskirat2357@gmail.com</p>
-            <div style={{marginLeft:"15%",marginTop:"-43%",marginBottom:"80%",fontSize:"200%",backgroundColor:"white",width:"4%",jusifyContent:"center",boxShadow:"0 0 3px 2px rgba(14, 13, 16, 0.5)",textAlign:"center"}} ><HiMail/></div>
+            <div style={{marginLeft:"15%",marginTop:"-43%",marginBottom:"80%",fontSize:"200%",backgroundColor:"white",width:"4%",jusifyContent:"center",boxShadow:"0 0 3px 2px rgba(14, 13, 16, 0.5)",textAlign:"center"}} className='bd' ><HiMail/></div>
 
    <div>
     
