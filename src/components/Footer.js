b1dcb13e27888f551a17ca49196d0d0fcb691af9
unknown <jaskirat2357@gmail.com>
2023-01-24 17:50:41 +0530

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import './Footer.css';
+import {FaLinkedin,FaGithubSquare,FaInstagramSquare,FaMailBulk,FaTelegramPlane ,FaFacebook} from "react-icons/fa";
 const Footer = () =>{
     return(
-        <div style={{backgroundColor:"rgba(43, 42, 45, 0.5)",width:"100%",paddingTop:"150px",marginTop:"-50%"}}>
-            <h1>hi</h1>
+        <div style={{backgroundColor:"rgba(20, 20, 21, 0.941)",width:"100%",paddingTop:"50px",marginTop:"-40%"}}>
+             <div className='icons'>
+    <FaLinkedin style={{color:"white",fontSize:"200%",marginLeft:"35%"}} className='icons1'/>
+     <FaGithubSquare style={{color:"white",fontSize:"200%",marginLeft:"3%"}} className='icons2'/>
+     <FaInstagramSquare style={{color:"white",fontSize:"200%",marginLeft:"3%"}} className='icons3'/>
+     <FaMailBulk style={{color:"white",fontSize:"250%",marginLeft:"3%"}} className='icons4'/>
+     <FaTelegramPlane style={{color:"white",fontSize:"200%",marginLeft:"3%"}}className='icons5'/>
+     <FaFacebook style={{color:"white",fontSize:"200%",marginLeft:"3%"}}className='icons6'/>
+     </div>
+    
         </div>
     )
 }
